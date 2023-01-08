e37b67c6646ce8e3cbcab93b3609027916328d6c
Pavel Tagankin <ptagankin@gmail.com>
2023-01-08 21:12:26 +0300

    дорабатывает последние ошибки

--- source/js/likes.js
+++ source/js/likes.js
@@ -24,38 +24,49 @@ const click_button = document.querySelectorAll('.card-info__counter');
 // }
 
 
-if (cards) {
-  cards.addEventListener ('click', function(event) {
-    let target = event.target;
+// if (cards) {
+//   cards.addEventListener ('click', function(event) {
+//     let target = event.target;
 
-    if (target.classList.contains('card-info__counter')){
-      if(target.classList.contains('added')) {
-          likes_number.textContent--;
-        } else {
-          likes_number.textContent++;
-        }
+//     if (target.classList.contains('card-info__counter')){
+//       if(target.classList.contains('added')) {
+//           likes_number.textContent--;
+//         } else {
+//           likes_number.textContent++;
+//         }
 
-        target.classList.toggle('added');
-    } else {
-      return;
-    };
-  });
-};
-for (let i = 0; i < click_button.length; i++) {
-  cards.addEventListener('click', (event) => {
+//         target.classList.toggle('added');
+//     } else {
+//       return;
+//     };
+//   });
+// };
+// for (let i = 0; i < click_button.length; i++) {
+//   cards.addEventListener('click', (event) => {
 
-    let current = event.target;
-    if (current.classList.contains('card-info__counter')) {
-      console.log('Всё работает!!!');
-      if (current.classList.contains('added')) {
-        let likesNumber = Number(likes_number.value);
-        likesNumber.textContent--;
-        likes_number = toString(likesNumber);
-      } else {
-        likes_number.textContent++;
-      }
+//     let current = event.target;
+//     if (current.classList.contains('card-info__counter')) {
+//       console.log('Всё работает!!!');
+//       if (current.classList.contains('added')) {
+//         let likesNumber = Number(likes_number.value);
+//         likesNumber.textContent--;
+//         likes_number = toString(likesNumber);
+//       } else {
+//         likes_number.textContent++;
+//       }
 
-      current.classList.toggle('added');
-    }
-  })
+//       current.classList.toggle('added');
+//     }
+//   })
+// }
+
+
+function showConsole() {
+  console.log("УРА!!!");
 }
+
+cards.addEventListener('click', function (event) {
+  if (event.target.closest('.card-info__counter')) {
+    showConsole();
+  }
+});
