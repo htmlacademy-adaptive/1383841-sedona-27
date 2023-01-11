2b898c8db57942fea59dfea14d703ba81776a9f2
Павел Таганкин <ptagankin@gmail.com>
2023-01-11 18:15:01 +0300

    дорабатывает ошибки

--- source/js/nav.js
+++ source/js/nav.js
@@ -4,10 +4,12 @@ let buttonNav = document.querySelector('.main-nav__toggle');
 let nav = document.querySelector('.main-nav__list');
 let disableJsFlex = document.querySelector('.no-js-flex');
 let disableJsNavButton = document.querySelector('.no-js-nav-button');
+let disableJsNav = document.querySelector('.no-js-nav');
 setTimeout(nav.classList.add('main-nav__list--tablet', 5000));
 
 disableJsFlex.classList.remove('no-js-flex');
 disableJsNavButton.classList.remove('no-js-nav-button');
+disableJsNav.classList.remove('no-js-nav');
 
 buttonNav.onclick = function () {
   nav.classList.toggle('opened-flex');
